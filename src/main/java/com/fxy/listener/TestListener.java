bf492821501abc815fc6d4c1521afc65a4d7a521
付东辉 <fstxizya@qq.com>
2022-04-09 10:24:09 +0800

    初始化

--- src/main/java/com/fxy/listener/TestListener.java
+++ src/main/java/com/fxy/listener/TestListener.java
@@ -1,7 +1,6 @@
 package com.fxy.listener;
 
 import com.fxy.Plugin;
-import net.mamoe.mirai.contact.Member;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 
 import java.util.function.Consumer;
@@ -15,10 +14,10 @@ public class TestListener implements Consumer<GroupMessageEvent> {
     @Override
     public void accept(GroupMessageEvent groupMessageEvent) {
 // 发消息
-        
+
 //        groupMessageEvent.getSender().sendMessage();
-        Member sender = groupMessageEvent.getSender();
-        if (sender.getId() == 352041906L) {
+        String senderName = groupMessageEvent.getSenderName();
+        if (senderName.equals("Fxy")) {
             Plugin.INSTANCE.getLogger().info("我自己说话了");
             System.out.println();
         } else {
